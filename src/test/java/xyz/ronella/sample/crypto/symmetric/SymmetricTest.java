7f257de0f1c627267eb4451e2dab9ea56ef1b291
Ron Webb <ron@ronella.xyz>
2023-04-21 09:20:52 +1200

    Symmetric testing.

---
+++ src/test/java/xyz/ronella/sample/crypto/symmetric/SymmetricTest.java
@@ -0,0 +1,73 @@
+package xyz.ronella.sample.crypto.symmetric;
+
+import org.junit.jupiter.api.*;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SymmetricTest {
+
+    private static String KEY;
+    private static final String PLAIN_TEXT = "THIS IS A SECRET";
+    private static String CIPER_TEXT;
+
+    @BeforeAll
+    public static void initialize() {
+        //The symmetric key to use for encryption and decryption.
+        KEY = generateKey();
+    }
+
+    private static String generateKey() {
+        try {
+            final var keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(256);
+            final var secretKey = keyGenerator.generateKey();
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static SecretKey decodeKey(final String encodedKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+        return new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+    }
+
+    @Test
+    @Order(10)
+    public void testAES256Encrypt() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        final var secret = decodeKey(KEY);
+
+        final var cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+
+        final var plaintextBytes = PLAIN_TEXT.getBytes(StandardCharsets.UTF_8);
+        final var ciphertextBytes = cipher.doFinal(plaintextBytes);
+        CIPER_TEXT = Base64.getEncoder().encodeToString(ciphertextBytes);
+
+        assertFalse(CIPER_TEXT.isEmpty());
+    }
+
+    @Test
+    @Order(20)
+    public void testAES256Decrypt() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        final var secretKey = decodeKey(KEY);
+
+        final var cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        final var decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(CIPER_TEXT));
+        final var decryptedText = new String(decryptedBytes, StandardCharsets.UTF_8);
+
+        assertEquals(PLAIN_TEXT, decryptedText);
+    }
+
+}
