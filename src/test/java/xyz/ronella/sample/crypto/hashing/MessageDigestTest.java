b2e9642755acb7968b6a73cc5d225f4015b22de9
Ron Webb <ron@ronella.xyz>
2023-04-24 16:29:15 +1200

    Don't handle the exception.

--- src/test/java/xyz/ronella/sample/crypto/hashing/MessageDigestTest.java
+++ src/test/java/xyz/ronella/sample/crypto/hashing/MessageDigestTest.java
@@ -12,29 +12,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MessageDigestTest {
 
     @Test
-    public void testSHA256() {
+    public void testSHA256() throws NoSuchAlgorithmException {
         final var password = "password123";
         final var algorithm = "SHA-256";
 
-        try {
-            final var digest = MessageDigest.getInstance(algorithm);
-            final var hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        final var digest = MessageDigest.getInstance(algorithm);
+        final var hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
 
-            // Convert the byte array into a hexadecimal string
-            final var hexString = new StringBuilderAppender();
+        // Convert the byte array into a hexadecimal string
+        final var hexString = new StringBuilderAppender();
 
-            for (final var b : hash) {
-                final var hex = Integer.toHexString(0xff & b);
-                hexString.append(()-> hex.length() == 1, "0").append(hex);
-            }
-
-            final var expectedHash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f";
-            final var hashOutput = hexString.toString();
-
-            assertEquals(expectedHash, hashOutput);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+        for (final var b : hash) {
+            final var hex = Integer.toHexString(0xff & b);
+            hexString.append(()-> hex.length() == 1, "0").append(hex);
         }
+
+        final var expectedHash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f";
+        final var hashOutput = hexString.toString();
+
+        assertEquals(expectedHash, hashOutput);
+
     }
 
 }
